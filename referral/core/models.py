a802d89680c177d51c9b606a9daf9e78b09c0490
Michael David <davidlearn27@gmail.com>
2023-03-14 06:12:42 +0100

    Created a User Manager

--- referral/core/models.py
+++ referral/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+import secrets
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, **other_fields):
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            **other_fields
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+
+
+
