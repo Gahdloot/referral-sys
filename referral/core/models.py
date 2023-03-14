7c8c636973095a3b56bafe7ad77cb06b2835bd38
Michael David <davidlearn27@gmail.com>
2023-03-14 06:13:28 +0100

    Created a User Class for account creation

--- referral/core/models.py
+++ referral/core/models.py
@@ -15,5 +15,26 @@ class UserManager(BaseUserManager):
         return user
 
 
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=255, default='')
+    last_name = models.CharField(max_length=255, default='')
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=15, blank=True)
+    company_name = models.CharField(max_length=15, blank=True)
+    is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "phone"]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 
