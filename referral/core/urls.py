a98bf07027e63d44af7546c6ef57d538c024c14c
Michael David <davidlearn27@gmail.com>
2023-03-28 23:41:37 +0100

    wrote the urls

--- referral/core/urls.py
+++ referral/core/urls.py
@@ -1,9 +1,8 @@
 from  django.urls import path
 from . import views
-# from rest_framework.authtoken import views
-
 
 urlpatterns = [
-    path('api-token-auth/', views.ObtainAuthToken.as_view(), name='login'),
+    path('api-token-auth/', views.LogInAPIView.as_view(), name='login'),
     path('api-logout/', views.LogoutAPIView.as_view(), name='api_logout'),
+    path('api-register/', views.RegisterAPIView.as_view(), name='api_register'),
 ]
